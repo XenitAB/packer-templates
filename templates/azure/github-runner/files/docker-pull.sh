b6e4bb009acea039a1ac3de65b3fca539359e91e
Simon Gottschlag <simon.gottschlag@xenit.se>
2020-12-04 14:13:46 +0100

    Pull the latest three tags

--- templates/azure/github-runner/files/docker-pull.sh
+++ templates/azure/github-runner/files/docker-pull.sh
@@ -2,3 +2,10 @@
 set -e
 
 docker pull ghcr.io/xenitab/github-actions/tools:latest
+
+set +e
+LATEST_TAGS=$(curl -s https://github.com/orgs/XenitAB/packages/container/github-actions%2Ftools/versions | grep "Label mr-1 mb-2 text-normal" | sed "s/.*<a .*>\(.*\)<\/a>/\1/g" | sort | tail -n 3)
+for TAG in ${LATEST_TAGS}; do
+    docker pull ghcr.io/xenitab/github-actions/tools:${TAG}
+done
+set -e
